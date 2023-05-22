0bbf1ae7982cbd28503c2f0ca45761e880e3db84
MadoXZA <timklo15@gmail.com>
2023-05-21 23:23:14 -0700

    Added paths

---
+++ paths.c
@@ -0,0 +1,86 @@
+#include "main.h"
+
+/**
+ * is_cmd - determines if a file is an executable command
+ * @info: info struct
+ * @path: path to file
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int is_cmd(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * dup_chars - duplicates characters
+ * @pathstr: PATH string
+ * @start: starting index
+ * @stop: stopping index
+ *
+ * Return: pointer to new buffer
+ */
+char *dup_chars(char *pathstr, int start, int stop)
+{
+	static char buf[1024];
+	int k = 0, l = 0;
+
+	for (l = 0, k = start; k < stop; k++)
+		if (pathstr[k] != ':')
+			buf[l++] = pathstr[l];
+	buf[l] = 0;
+	return (buf);
+}
+
+/**
+ * find_path - finds this cmd in PATH string
+ * @info: info struct
+ * @pathstr: PATH string
+ * @cmd: cmd to find
+ *
+ * Return: full path of cmd if found or NULL
+ */
+char *find_path(info_t *info, char *pathstr, char *cmd)
+{
+	int k = 0, curr_pos = 0;
+	char *path;
+
+	if (!pathstr)
+		return (NULL);
+	if ((_strlen(cmd) > 2) && starts_with(cmd, "./"))
+	{
+		if (is_cmd(info, cmd))
+			return (cmd);
+	}
+	while (1)
+	{
+		if (!pathstr[k] || pathstr[k] == ':')
+		{
+			path = dup_chars(pathstr, curr_pos, k);
+			if (!*path)
+				_strcat(path, cmd);
+			else
+			{
+				_strcat(path, "/");
+				_strcat(path, cmd);
+			}
+			if (is_cmd(info, path))
+				return (path);
+			if (!pathstr[i])
+				break;
+			curr_pos = k;
+		}
+		k++;
+	}
+	return (NULL);
+}
